35acc9908088828e941a5b134c0029f9154f95c7
oscar <oscarf200106@gmail.com>
2024-04-08 18:10:19 +0000

    craete-package update

---
+++ tools/deployer/create-package.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once('rabbitMQLib.inc');
+
+$connectionConfig = [
+    "BROKER_HOST" => "deployer",
+    "BROKER_PORT" => 5672,
+    "USER" => "bookQuest",
+    "PASSWORD" => "8bkJ3r4dWSU1lkL6HQT7",
+    "VHOST" => "bookQuest",
+];
+
+$exchangeQueueConfig = [
+    "EXCHANGE_TYPE" => "topic",
+    "AUTO_DELETE" => true,
+    "EXCHANGE" => "createPackageExchange",
+    "QUEUE" => "createPackageQueue"
+];
+
+// Fetch arguments
+$environment = $argv[1];
+$file_location = $argv[2];
+
+$request = [
+    "type" => $type,
+    "environment" => $environment,
+    "file_location" => $file_location
+];
+
+// Sends the message and waits for a response
+$client = new rabbitMQClient($connectionConfig, $exchangeQueueConfig);
+$response = $client->send_request($request);
+
+// Returns response
+header("Content-Type: application/json");
+http_response_code($response["returnCode"]);
+echo json_encode($response);
+exit(0);
--- tools/deployer/create-package.sh
+++ tools/deployer/create-package.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-# Get the absolute path of the script
-TARGET_DIR="./*"
+# Check if the first argument is set
+if [ -z "$1" ]; then
+    echo "Error: Command argument missing."
+    exit 1
+fi
 
-# Get the parent directory of the parent directory of the script
+# Get the absolute path of the script directory
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+# Get the directory three levels up from the script directory
+TARGET_DIR=$(dirname $(dirname $(dirname "$SCRIPT_DIR")))
 
 # Create a timestamp for the zip file
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 
 # Create a backup directory if it doesn't exist
-PACKAGE_DIR="./packages/backup"
+PACKAGE_DIR="$SCRIPT_DIR/packages/backup"
 
-# Create the zip file in the backup directory
-zip -r "$PACKAGE_DIR/backup_$TIMESTAMP.zip" "$TARGET_DIR"
-echo $TARGET_DIR
+# Change directory to the target directory
+cd "$TARGET_DIR" || { echo "Error: Unable to change directory to $TARGET_DIR"; exit 1; }
 
-echo "Zip file created in $PACKAGE_DIR"
+# Create the zip file in the backup directory, skipping the 'packages' directory
+ZIP_FILE="$PACKAGE_DIR/backup_$TIMESTAMP.zip"
+zip -r "$ZIP_FILE" IT490-Project -x "IT490-Project/tools/deployer/packages/*"
+
+echo "Zip file created in $ZIP_FILE"
+
+# Run create-package.php with arguments
+php "$SCRIPT_DIR/create-package.php" "$1" "$ZIP_FILE"
