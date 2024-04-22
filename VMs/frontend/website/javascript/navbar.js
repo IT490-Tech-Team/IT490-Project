f833b6e84a8ede42ac6d047f999add6bd5a390b1
ofelizestevez <oscarf201006@gmail.com>
2024-04-22 21:36:10 +0000

    formatting navbar.js

--- VMs/frontend/website/javascript/navbar.js
+++ VMs/frontend/website/javascript/navbar.js
@@ -22,6 +22,31 @@ const logOut = (e) => {
     location.reload()
 }
 
+const addServerChanger = (container) => {
+    let currentServer = getCookies(SERVER_COOKIE_NAME)
+    
+    // If the server cookie is set, then it's safe to show the user the cookie changer.
+    // If it isn't set, then they don't need to change servers
+    if (currentServer){
+        currentServer = SERVERS[0]
+
+        const serverChangerLink = document.createElement("a")
+        serverChangerLink.textContent = currentServer
+        serverChangerLink.href = ""
+        serverChangerLink.addEventListener("click", () => {
+            e.preventDefault()
+            // It takes the index of currentServer, adds 1, then gets the value at this new index
+            // If this is undefined (i.e. new index is too much), it's going to default to the first server on the list.
+            const newServer = SERVERS[SERVERS.indexOf(currentServer) + 1] ?? SERVERS[0] 
+    
+            setCookies(SERVER_COOKIE_NAME, newServer)
+        })
+        
+        container.appendChild(serverChangerLink)
+    }
+
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const nav = document.querySelector("nav")
 
@@ -42,39 +67,16 @@ document.addEventListener("DOMContentLoaded", () => {
     addPath("Register","/register",right)
     
     if(getCookies(SESSION_ID_COOKIE_NAME)){
-        const search = addPath("Search","/search",center)
-        const library = addPath("Library","/library",center)
-        const recommendations = addPath("Recommendations", "/recommendations", center)
-        const email = addPath("Email", "/email", center)
-
+        addPath("Search","/search",center)
+        addPath("Library","/library",center)
+        addPath("Recommendations", "/recommendations", center)
+        addPath("Email", "/email", center)
 
         const logout = addPath("Log Out", "/logout", right)
         logout.addEventListener("click", (e) => {  logOut(e) })
     }
 
-    // Add Server Changer
-    const serverChangerLink = document.createElement("a")
-    let currentServer = getCookies(SERVER_COOKIE_NAME)
-
-    // If currentServer is undefined or null, it defaults to the last element in servers array
-    if (!currentServer){
-        currentServer = SERVERS[0]
-    }
-    serverChangerLink.textContent = currentServer
-    serverChangerLink.href = ""
-    
-    serverChangerLink.addEventListener("click", () => {
-        e.preventDefault()
-        // It takes the index of currentServer, adds 1, then gets the value at this new index
-        // If this is undefined (i.e. new index is too much), it's going to default to the first server on the list.
-
-        const newServer = SERVERS[SERVERS.indexOf(currentServer) + 1] ?? SERVERS[0] 
-
-        setCookies(SERVER_COOKIE_NAME, newServer)
-    })
-    
-    right.appendChild(serverChangerLink)
-
+    addServerChanger(right)
 
     // Add areas to nav bar
     nav.appendChild(left)
