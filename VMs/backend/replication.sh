83a8b06bd4133b63dceb4a52538b0fe71fa05082
ofelizestevez <34465526+ofelizestevez@users.noreply.github.com>
2024-04-24 22:47:04 -0400

    differentiates between main and backup to set domain and log path

--- VMs/backend/replication.sh
+++ VMs/backend/replication.sh
@@ -9,7 +9,7 @@ setup_master() {
   # Enable Binary Logging
   echo "[mysqld]" | sudo tee -a /etc/mysql/my.cnf
   echo "server-id = 1" | sudo tee -a /etc/mysql/my.cnf
-  echo "log_bin = /var/log/mysql/mysql-bin.log" | sudo tee -a /etc/mysql/my.cnf
+  echo "log_bin = $log_bin_path" | sudo tee -a /etc/mysql/my.cnf
 
   # Restart MySQL Service
   sudo service mysql restart
@@ -30,18 +30,16 @@ setup_slave() {
     exit 1
   fi
 
-  # MySQL Configuration
-  MASTER_IP="$1"
-
   # Enable Binary Logging
   echo "[mysqld]" | sudo tee -a /etc/mysql/my.cnf
   echo "server-id = 2" | sudo tee -a /etc/mysql/my.cnf
+  echo "log_bin = $log_bin_path" | sudo tee -a /etc/mysql/my.cnf
 
   # Restart MySQL Service
   sudo service mysql restart
 
   # Connect Slave to Master
-  sudo mysql -e "CHANGE MASTER TO MASTER_HOST='$MASTER_IP', MASTER_USER='$REPLICATION_USER', MASTER_PASSWORD='$REPLICATION_PASSWORD';"
+  sudo mysql -e "CHANGE MASTER TO MASTER_HOST='$master_domain', MASTER_USER='$REPLICATION_USER', MASTER_PASSWORD='$REPLICATION_PASSWORD';"
 
   # Start Replication
   sudo mysql -e "START SLAVE;"
@@ -51,21 +49,35 @@ setup_slave() {
 }
 
 # Check if the required parameters are provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <relationship> [<parent_ip>]"
-  echo "Relationship should be 'parent' or 'child'"
+if [ -z "$1" ] || [ -z "$2" ]; then
+  echo "Usage: $0 <machinetype> <relationship>"
+  echo "Example: $0 main parent"
+  exit 1
+fi
+
+MACHINE_TYPE="$1"
+RELATIONSHIP="$2"
+
+if [ "$MACHINE_TYPE" == "main" ]; then
+  log_bin_path="/var/log/mysql/mysql-bin-main.log"
+  master_domain="prod-backend"
+elif [ "$MACHINE_TYPE" == "backup" ]; then
+  log_bin_path="/var/log/mysql/mysql-bin-backup.log"
+  master_domain="prod-backend-backup"
+else
+  echo "Invalid machine type. Use 'main' or 'backup'."
   exit 1
 fi
 
 # Check the relationship parameter
-if [ "$1" == "parent" ]; then
+if [ "$RELATIONSHIP" == "parent" ]; then
   setup_master
-elif [ "$1" == "child" ]; then
-  if [ -z "$2" ]; then
+elif [ "$RELATIONSHIP" == "child" ]; then
+  if [ -z "$3" ]; then
     echo "Usage: $0 child <parent_ip>"
     exit 1
   fi
-  setup_slave "$2"
+  setup_slave "$3"
 else
   echo "Invalid relationship parameter. It should be 'parent' or 'child'."
   exit 1
